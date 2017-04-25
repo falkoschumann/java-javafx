e66cbf1ebb18280b558d2253de880bef06cff52c
Falko Schumann <falko.schumann@muspellheim.de>
2017-04-25 22:09:20 +0200

    Create project

---
+++ README.md
@@ -0,0 +1,2 @@
+JavaFX View Controller
+======================
---
+++ src/main/java/de/muspellheim/javafx/viewcontroller/ViewController.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2017 Falko Schumann
+ * Released under the terms of the MIT License.
+ */
+
+package de.muspellheim.javafx.viewcontroller;
+
+public class ViewController {
+}
